ac3ff5a8e6bba63fe68323daa720af3e1ca8bd7b
Vitaly Litvinovich <witodivaro@gmail.com>
2021-05-07 16:08:19 +0300

    feat: handle 404

--- src/controllers/404.js
+++ src/controllers/404.js
@@ -1,3 +1,7 @@
+const { ROUTE_NOT_FOUND } = require("../consts/serverErrors");
+const { StatusCodes } = require("../utils/codes");
+const { NotFoundError } = require("../utils/errors");
+
 exports.get404 = (req, res) => {
-  res.status(404).send();
+  res.status(StatusCodes.NOT_FOUND).send(new NotFoundError(ROUTE_NOT_FOUND));
 };
